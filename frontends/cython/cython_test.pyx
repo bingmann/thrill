9952dbcfb59446e417536e52d15a77b99c927c0b
Timo Bingmann <tbgit@panthema.net>
2015-10-04 06:06:35 +0200

    Add cmake scripts and magic to compile cython stub program.

---
+++ frontends/cython/cython_test.pyx
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+##########################################################################
+# frontends/swig_python/python_test.py
+#
+# Part of Project Thrill.
+#
+# Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+#
+# All rights reserved. Published under the BSD-2 license in the LICENSE file.
+##########################################################################
+
+print("hello from python")
+
+##########################################################################
