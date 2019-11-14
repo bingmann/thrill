ad6ea494625a0d923417283c2d1fa5ff05560c50
Timo Bingmann <tbgit@panthema.net>
2019-11-14 14:00:57 +0100

    submit thrill python interface 0.0.1 to pypi

---
+++ frontends/python/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+##########################################################################
+# frontends/python/tests/__init__.py
+#
+# Part of Project Thrill - http://project-thrill.org
+#
+#
+# All rights reserved. Published under the BSD-2 license in the LICENSE file.
+##########################################################################
